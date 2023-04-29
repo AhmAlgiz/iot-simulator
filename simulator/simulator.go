c99ee6d9ee8d523ecf16b65d7888476f7e751317
AhmAlgiz <97907492+AhmAlgiz@users.noreply.github.com>
2023-04-29 22:09:26 +0300

    test set

--- simulator/simulator.go
+++ simulator/simulator.go
@@ -6,16 +6,22 @@ import (
 	"time"
 )
 
-func Simulate(client client.Client) {
-	hygrometer := device.CreateDevice(client, 40, 40, "base/state/humidity")
-	termometer := device.CreateDevice(client, 25, 25, "base/state/temperature")
+func Simulate(c client.Client) {
+	hygrometer := device.CreateDevice(c, 40, 40, "base/state/humidity")
+	termometer := device.CreateDevice(c, 25, 25, "base/state/temperature")
 
 	heaterTopics := [2]string{"base/state/heater", "base/state/heater-temp"}
 	condTopics := [3]string{"base/state/conditioner", "base/state/conditioner-temp", "base/state/conditioner-hym"}
-	heater := device.CreateAppliance(client, heaterTopics[:])
-	conditioner := device.CreateAppliance(client, condTopics[:])
+	heater := device.CreateAppliance(c, heaterTopics[:])
+	conditioner := device.CreateAppliance(c, condTopics[:])
 	heater.Subscribe()
 	conditioner.Subscribe()
+
+	//publish default values for testing
+	client.Publish(c, "base/state/temperature-point", "25")
+	client.Publish(c, "base/state/humidity-point", "40")
+	client.Publish(c, "base/state/conditioner", "1")
+	client.Publish(c, "base/state/heater", "0")
 
 	for {
 		hygrometer.Generate()
